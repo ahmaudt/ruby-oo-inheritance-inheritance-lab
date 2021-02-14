6c42bcefc8f7bf0c1df11180964b51d50cea548d
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-02-14 17:21:21 -0500

    commit tree e60f9a6de5b48e672f6f19bb377c07049215cb5b

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
+
+    @knowledge = []
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(virtue)
+        @knowledge << virtue
+    end
 
 end
